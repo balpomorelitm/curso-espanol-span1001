0b1b29d93efbf2142cd38900e1d229a1c614f634
balpomorelitm <pablotorrado@gmail.com>
2025-11-28 16:17:11 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   srcDir: 'src',
+  // IMPORTANTE: Configuración para GitHub Pages
+  site: 'https://balpomorelitm.github.io', // Tu usuario
+  base: '/curso-espanol-span1001',         // El nombre de tu repositorio
 });
